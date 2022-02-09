fd856fac07d32e8bd24206a7400832de97c73d3b
Joshua Peisach <itzswirlz2020@outlook.com>
2022-02-09 13:31:19 -0500

    typo

--- src/main/java/com/github/ItzSwirlz/BTEServerSwitcher/BTECommand.java
+++ src/main/java/com/github/ItzSwirlz/BTEServerSwitcher/BTECommand.java
@@ -20,7 +20,7 @@ import net.minecraft.util.text.TextComponentString;
 
 public class BTECommand extends CommandBase {
 
-	List alises = new ArrayList();
+	List aliases = new ArrayList();
 	aliases.add("buildtheearth");
 
 	public String[] ips = {"bte",
