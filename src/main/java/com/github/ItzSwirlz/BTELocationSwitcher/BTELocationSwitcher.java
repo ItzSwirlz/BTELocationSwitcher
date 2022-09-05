a89cb6c9e3545ce5dc543a0ff1aafc87e0e959f6
Joshua Peisach <itzswirlz2020@outlook.com>
2022-09-05 17:07:53 -0400

    Release 1.0.0 for 1.12.2

--- src/main/java/com/github/ItzSwirlz/BTELocationSwitcher/BTELocationSwitcher.java
+++ src/main/java/com/github/ItzSwirlz/BTELocationSwitcher/BTELocationSwitcher.java
@@ -11,7 +11,7 @@ public class BTELocationSwitcher
 {
     public static final String MODID = "btelocationswitcher";
     public static final String NAME = "Build The Earth Location Switcher";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
